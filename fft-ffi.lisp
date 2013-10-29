23abbf06267dc2742aafe53899c5c71d134df76f
Martin Kielhorn <kielhorn.martin@gmail.com>
2013-10-29 10:45:51 +0000

    fouriertransform of a quadratic 2d image seems to work

--- fft-ffi.lisp
+++ fft-ffi.lisp
@@ -8,14 +8,18 @@
 (defconstant +measure+ 0)
 (defconstant +estimate+ (ash 1 6)) ;; array isn't overwritten during planning
 
-(load-shared-object "/usr/local/lib/libfftw3f.so.3")
+(load-shared-object "libfftw3.so.3")
 (define-alien-type plan (* int))
 
 ;; power of two fastest; 2 3 5 7 11 13 should be the only dividors,
 ;; then fast algorithms are available; it is benificial if last
 ;; dimension of r2c/c2r transform should be even
 
-(define-alien-routine fftwf_execute
+(define-alien-routine fftw_execute
+    void
+  (plan plan))
+
+(define-alien-routine fftw_destroy_plan
     void
   (plan plan))
 
@@ -40,63 +44,101 @@
 
 ;; this is how multidimensional arrays are stored in lisp
 
-(define-alien-routine fftwf_plan_dft_r2c
+(define-alien-routine fftw_plan_dft_r2c
     plan
   (rank int)
   (n (* int))
-  (in (* single-float))
-  (out (* single-float)) ;; actually complex
+  (in (* double-float))
+  (out (* double-float)) ;; actually complex
   (flags unsigned-int))
 
-(define-alien-routine fftwf_plan_dft_c2r
+(define-alien-routine fftw_plan_dft_c2r
     plan
   (rank int)
   (n (* int))
-  (in (* single-float)) ;; actually complex
-  (out (* single-float)) 
+  (in (* double-float)) ;; actually complex
+  (out (* double-float)) 
   (flags unsigned-int))
 
-(load-shared-object "/usr/local/lib/libfftw3f_threads.so")
+
+(define-alien-routine fftw_plan_dft
+    plan
+  (rank int)
+  (n (* int))
+  (in (* double-float))	 ;; actually complex
+  (out (* double-float))	  ;; actually complex
+  (flags unsigned-int))
+
+
+(load-shared-object "libfftw3_threads.so.3")
   
-(define-alien-routine ("fftwf_init_threads" init-threads)
+(define-alien-routine ("fftw_init_threads" init-threads)
     int)
 
-(define-alien-routine ("fftwf_plan_with_nthreads" plan-with-nthreads)
+(define-alien-routine ("fftw_plan_with_nthreads" plan-with-nthreads)
     void
   (nthreads int))
 
 #+nil
 (progn
  (init-threads)
- (plan-with-nthreads 2))
+ (plan-with-nthreads 4))
 
 
-(defun plan (dims a)
-  (declare ((simple-array single-float *) a))
-  (let* ((rank (array-rank a))
-	 (dims-a (make-array rank :element-type '(signed-byte 32)
-			     :initial-contents dims))
-	 (a-sap (sb-sys:vector-sap
-		 (sb-ext:array-storage-vector a))))
-    (sb-sys:with-pinned-objects (dims-a a)
-      (fftwf_plan_dft_r2c rank (sb-sys:vector-sap dims-a)
-			  a-sap a-sap +measure+))))
+(defun plan (in &optional out)
+  (declare (type (array (complex double-float) *) in))
+  (let* ((in-d (array-displacement in))
+	 (out-d (array-displacement (if out out in))))
+    (if (not (and in-d out-d))
+	(error "initially you should allocate data as a 1d array in lisp and then use displacement.")
+     (let* ((rank (array-rank in))
+	    (dims-in (make-array rank :element-type '(signed-byte 32)
+				 :initial-contents (array-dimensions in)))
+	    (in-sap (sb-sys:vector-sap
+		     (sb-ext:array-storage-vector in-d)))
+	    (out-sap (sb-sys:vector-sap
+		      (sb-ext:array-storage-vector out-d))))
+       (format t "array alignment ~a" (list in-sap out-sap))
+       (sb-sys:with-pinned-objects (dims-in in out)
+	 (fftw_plan_dft rank (sb-sys:vector-sap dims-in)
+			in-sap out-sap +estimate+))))))
 
-(defun ft (plan a)
-  (declare ((simple-array single-float *) a))
+(defun ft-inplace (a)
+  (declare (type (array (complex double-float) *) a))
   (sb-sys:with-pinned-objects (a)
-    (fftwf_execute plan)))
+    (let ((plan (plan a)))
+      (fftw_execute plan)))
+  a)
+
+(defun ft (in)
+  (declare (type (array (complex double-float) *) in))
+  (let* ((out1 (make-array (reduce #'* (array-dimensions in))
+			   :element-type '(complex double-float)))
+	 (out (make-array (array-dimensions in)
+			  :element-type '(complex double-float)
+			  :displaced-to out1)))
+    (sb-sys:with-pinned-objects (in out)
+      (let ((plan (plan in out)))
+	(fftw_execute plan)))
+    out))
+
 #+nil
-(let* ((n0 128)
-       (n1 128)
-       (n2 128)
-       (n3 128)
-       (a (make-array (list (* 2 (+ (floor n3 2) 1)) n2 n1 n0)
-		      :element-type 'single-float))
-       (plan (time (plan a))))
-  (defparameter *plan* plan)
-  (time (ft plan a))
-  (aref a 0 0 0 0))
+(time
+ (let* ((n0 512)
+	(n1 n0)
+	(in1 (make-array (* n1 n0)
+			 :element-type '(complex double-float)
+			 :initial-element (complex 1d0 0d0)))
+	(in (make-array (list n1 n0)
+			:element-type '(complex double-float)
+			:displaced-to in1))
+	(out (ft in)))
+   (aref out 0 0)))
+
+
+
+;; new laptop with 4 processors and with complex double-float 
+;; 8192x8192 2.9s
 
 
 ;; 1024x1024x512 transform takes 21s
@@ -127,38 +169,67 @@
         (write-sequence data1 s)))
     nil))
 
-(defun draw-disk (img radius y x)
-  (declare (single-float radius y x)
-	   ((array single-float *) img)
-	   (values (array single-float *) &optional))
-  (destructuring-bind (h w) (array-dimensions img)
-    (declare ((integer 0 65535) w h))
-    (let ((rr (* radius radius))
-	  (w2 (* .5 w))
-	  (h2 (* .5 h)))
-     (dotimes (j h)
-       (dotimes (i w)
-	 (let* ((x (- i x w2))
-		(y (- j y h2))
-		(r2 (+ (* x x) (* y y))))
-	   (setf (aref img j i)
-		 (if (< r2 (* 2 rr))
-		     (* 255s0 (exp (* .01s0 (- r2))))
-		     0s0)))))))
-  img)
+(defun read-pgm (filename)
+  (declare ((or pathname string) filename)
+           (values (array (unsigned-byte 8) 2) &optional))
+  (with-open-file (s filename)
+    (unless (equal (symbol-name (read s)) "P5")
+      (error "no PGM file"))
+    (let* ((w (read s))
+           (h (read s))
+           (grays (read s))
+           (pos (file-position s))
+           (data (make-array (list h w)
+                             :element-type '(unsigned-byte 8)))
+           (data-1d (make-array (* h w)
+                                :element-type '(unsigned-byte 8)
+                                :displaced-to data)))
+      (declare ((integer 0 65535) grays w h))
+      (unless (= grays 255)
+        (error "image has wrong bitdepth"))
+      (with-open-file (s filename
+                         :element-type '(unsigned-byte 8))
+        (file-position s pos)
+        (read-sequence data-1d s))
+      data)))
+
+#+nil
+(defparameter *lena8*
+ (read-pgm "~/ref_orig.pgm"))
+
+(defparameter *klena*
+  (destructuring-bind (h w) (array-dimensions *lena8*)
+   (let* ((a1 (make-array (* h w) :element-type '(complex double-float)))
+	  (a (make-array (list h w) :element-type '(complex double-float)
+			 :displaced-to a1))
+	  (l1 (sb-ext:array-storage-vector *lena8*)))
+     (dotimes (i (length a1))
+       (setf (aref a1 i) (complex (* 1d0 (aref l1 i)))))
+     (ft a))))
+
+(defparameter *klena8*
+ (destructuring-bind (h w) (array-dimensions *klena*)
+   (let* ((a (make-array (list h w) :element-type '(unsigned-byte 8)))
+	  (a1 (sb-ext:array-storage-vector a))
+	  (l1 (sb-ext:array-storage-vector (array-displacement *klena*))))
+     (dotimes (i (length a1))
+       (setf (aref a1 i) (min 255 (max 0 (floor (* 28 (log (+ 1 (abs (aref l1 i))))))))))
+     a)))
+
+(write-pgm "klena.pgm" *klena8*)
 
 (defun clamp (x)
-  (declare (single-float x)
+  (declare (type double-float x)
 	   (values fixnum &optional))
-  (cond ((< x 0s0) 0)
-	((< 255s0 x) 255)
+  (cond ((< x 0d0) 0)
+	((< 255d0 x) 255)
 	(t (multiple-value-bind (a b) (floor x)
 	     (declare (ignore b))
 	     a))))
 
 (defun convert (img)
-  (declare ((array single-float *) img))
-  (let* ((img1 (make-array (array-total-size img) :element-type 'single-float
+  (declare (type (array double-float *) img))
+  (let* ((img1 (make-array (array-total-size img) :element-type 'double-float
 			   :displaced-to img))
 	 (n (length img1))
 	 (out (make-array (array-dimensions img)
@@ -168,12 +239,12 @@
       (setf (aref out1 i) (clamp (aref img1 i))))
     out))
 
-(defun normalize (img &optional (value 255s0))
-  (declare ((array single-float *) img)
-	   (single-float value)
-	   (values (array single-float *) &optional))
+(defun normalize (img &optional (value 255d0))
+  (declare (type (array double-float *) img)
+	   (double-float value)
+	   (values (array double-float *) &optional))
   (let* ((img1 (make-array (array-total-size img)
-			   :element-type 'single-float
+			   :element-type 'double-float
 			   :displaced-to img))
 	 (mi (reduce #'min img1))
 	 (ma (reduce #'max img1))
@@ -183,96 +254,3 @@
       (setf (aref img1 i)
 	    (* s (- (aref img1 i) mi))))
     img))
-
-#+nil
-(let* ((al (list 1s0 1.1s0 2s0 3s0))
-       (a (make-array (length al)
-		      :element-type 'single-float
-		      :initial-contents al)))
-  (normalize a))
-
-(defconstant +pi+ (coerce pi 'single-float))
-
-(defun complexify (dims a)
-  (declare ((array single-float *) a))
-  (destructuring-bind (z y x) dims
-    (let* ((xp (array-dimension a 2))
-	   (xp2 (floor xp 2))
-	   (o (make-array (list z y xp2) 
-			  :element-type '(complex single-float))))
-      (dotimes (k z)
-	(dotimes (j y) 
-	  (dotimes (i xp2)
-	    (setf (aref o k j i) (complex (aref a k j (* 2 i))
-					  (aref a k j (1+ (* 2 i))))))))
-      o)))
-
-(defun xz-slice (a y)
-  (declare ((array * *) a)
-	   (fixnum y))
-  (destructuring-bind (z yy x) (array-dimensions a)
-    (declare (ignore yy))
-   (let ((slice (make-array (list z x)
-			    :element-type (array-element-type a))))
-     (dotimes (k z)
-       (dotimes (i x)
-	 (setf (aref slice k i) 
-	       (aref a k y i))))
-     slice)))
-
-(defun xy-slice (a z)
-  (destructuring-bind (zz y x) (array-dimensions a)
-    (declare (ignore zz))
-    (make-array (list y x) 
-		:element-type (array-element-type a)
-		:displaced-to a
-		:displaced-index-offset (* z y x))))
-
-#+nil
-(let* ((z 128)
-       (w 128)
-       (wp2 (+ (floor w 2) 1))
-       (wp (* 2 wp2))
-       (h 128)
-       (vol (make-array (list z h wp)
-			 :element-type 'single-float)))
-  (sb-sys:with-pinned-objects (vol)
-    (dotimes (i z)
-      (let ((a (make-array (list h wp)
-			   :element-type 'single-float
-			   :displaced-to vol
-			   :displaced-index-offset (* wp  h i)))
-	    (z (* 32s0 (exp (complex 0 (* +pi+ i (/ 2s0 z)))))))
-	(draw-disk a 12.4s0 (realpart z) (imagpart z))))
-
-    (ft (plan (list z h w) vol) vol)
-    
-    (let* ((com (complexify (list z h w) vol))
-	   (com1 (sb-ext:array-storage-vector com))
-	   (coma (make-array (list z h wp2) :element-type 'single-float))
-	   (coma1 (sb-ext:array-storage-vector coma)))
-      (dotimes (i (* z h wp2))
-	(setf (aref coma1 i)	(log (+ .01s0 (abs (aref com1 i))))))
-      (normalize coma 255 s0)
-      (dotimes (j 4)
-	(write-pgm (format nil "/dev/shm/o~3,'0d.pgm" j)
-		   (convert (xz-slice coma j)))))))
-
-#+nil
-(let* ((w 134)
-       (h 129)
-       (a (make-array (list h (* 2 (1+ (floor w 2))))
-		     :element-type 'single-float)))
-  (draw-disk a 14.3s0 0s0 0s0)
-  (ft (plan (list h w) a) a)
-  (let ((b (make-array (list h
-			     (1+ (floor w 2))
-			     )
-		       :element-type 'single-float)))
-    (destructuring-bind (h w) (array-dimensions b)
-      (dotimes (j h)
-	(dotimes (i w)
-	  (setf (aref b j i) (log (abs (complex (aref a j (* 2 i))
-					    (aref a j (1+ (* 2 i))))))))))
-    (normalize b)
-    (write-pgm "/dev/shm/o.pgm" (convert b))))
